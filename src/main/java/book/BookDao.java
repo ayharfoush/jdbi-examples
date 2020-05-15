4a28a7c656d883c3cfe57f2ceb3463c1592fa091
ayharfoush <61536045+ayharfoush@users.noreply.github.com>
2020-05-15 10:23:56 +0200

    Add files via upload

---
+++ src/main/java/book/BookDao.java
@@ -0,0 +1,45 @@
+package book;
+
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RegisterBeanMapper(Book.class)
+public interface BookDao {
+    @SqlUpdate("""
+        CREATE TABLE book (
+            isbn13 VARCHAR PRIMARY KEY,
+            author VARCHAR NOT NULL,
+            title VARCHAR NOT NULL,
+            format VARCHAR NOT NULL,
+            publisher VARCHAR NOT NULL,
+            publicationDate VARCHAR NOT NULL,
+            pages INTEGER NOT NULL,
+            available BOOLEAN NOT NULL
+        )
+        """
+    )
+    void createTable();
+
+    @SqlUpdate("INSERT INTO book VALUES (:isbn13, :author, :title, :format, :publisher,:publicationDate,:pages,:available)")
+    void insert(@Bind("isbn13") String isbn13, @Bind("author") String author, @Bind("title") String title, @Bind("format") Book.Format format,
+                @Bind("publisher") String publisher, @Bind("publicationDate") LocalDate publicationDate,@Bind("pages") int pages, @Bind("available") boolean available);
+
+    @SqlUpdate("INSERT INTO book VALUES (:isbn13, :author, :title, :format, :publisher,:publicationDate,:pages,:available)")
+    void insert(@BindBean Book book);
+
+    @SqlQuery("SELECT * FROM book WHERE isbn13 = :isbn13")
+    Optional<Book> find(@Bind("isbn13") String isbn13);
+
+    @SqlUpdate("DELETE FROM book WHERE isbn13= :isbn13")
+    void delete(@BindBean Book book);
+
+    @SqlQuery("SELECT * FROM book ORDER BY publicationDate")
+    List<Book> findAll();
+}
---
+++ src/main/java/book/Main.java
@@ -0,0 +1,19 @@
+package book;
+
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test");
+        try (Handle handle = jdbi.open()) {
+            BookDao dao = handle.attach(BookDao.class);
+            dao.createTable();
+            dao.delete(book);
+            dao.findAll().stream().map(book::getpublicationdate).sorted().forEach(System.out::println);
+    }
+}
